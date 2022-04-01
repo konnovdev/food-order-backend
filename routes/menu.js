5f58106535a51f42ff524ce8dcf6d1c25772b233
ilya k <konnovdev@gmail.com>
2022-04-02 01:01:59 +0800

    Added swagger documentation generator

--- routes/menu.js
+++ routes/menu.js
@@ -3,6 +3,22 @@ import {get} from '../controllers/menu.js'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /api/menu/{restaurantId}:
+ *   get:
+ *     summary: Get the entire menu of a restaurant by restaurant ID
+ *     parameters:
+ *       - in: path
+ *         name: restaurantId
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Numeric id of the restaurant
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/api/menu/:id', get)
 
 export default router
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,6 +3,15 @@ import {getAll} from '../controllers/restaurants.js'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /api/restaurants:
+ *   get:
+ *     summary: Get a list of nearby restaurants
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/api/restaurants', getAll)
 
 export default router
