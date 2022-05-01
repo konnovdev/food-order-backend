0117f57f2f78eac8ddcdfc92abc33234c92cfd09
ilya k <konnovdev@gmail.com>
2022-05-02 01:59:54 +0800

    Fixed an error in one of the sql scripts

--- db/data/queries/comment.sql
+++ db/data/queries/comment.sql
@@ -7,4 +7,4 @@ INSERT INTO `Comment` VALUES('12345', "fat man", "awesome food", "2022-04-11T14:
 INSERT INTO `Comment` VALUES('123', "evil spirit", "DO NOT order this", "2022-04-11T14:01:18");
 INSERT INTO `Comment` VALUES('12', "", "order this please", "2022-04-11T14:01:18");
 
-SELECT * FROM `COMMENT`;
+SELECT * FROM `Comment`;
