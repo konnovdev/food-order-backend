0bc833be47fc3a908b6275a4ff8d45c6d0c32817
ilya k <konnovdev@gmail.com>
2022-05-22 18:15:38 +0800

    Delete unused mock data

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,19 +1,4 @@
-// frontend staffs give us only one menu example, I assign it to menuResTwo. 
-import {menuResOne, menuResTwo} from "../db/db.js"
-import {dbQuery} from "../db/connection.js"
 import {queryAllItem} from "../db/utility.js"
-
-function getById(id) {
-    if (id === '1') {
-        return menuResOne
-    }
-    else if (id === '2') {
-        return menuResOne
-    }
-    else {
-        return null
-    }
-}
 
 const get = async (req, res) => {
     let result = await queryAllItem()
