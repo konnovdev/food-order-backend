e2520372c687dc32ab12b5e9096aae116bea1070
ilya k <konnovdev@gmail.com>
2022-05-22 20:12:00 +0800

    Add English translation of items (Used google translate)

--- db/data/queries/insertItem.sql
+++ db/data/queries/insertItem.sql
@@ -25,5 +25,17 @@ INSERT INTO `Item_Trans` VALUES('7','zh', "泡芙", "看似一大圈的柔滑奶
 INSERT INTO `Item_Trans` VALUES('8','zh', "洋蔥圈", "洋蔥圈並不是火辣辣的，而是淡淡的香味，每次吃都感覺一股淡淡的鹹味和洋蔥味，兩種味道夾雜在一起，加上一些些作料和碳水化合物，那味道簡直無法形容", "點心", 'item008');
 INSERT INTO `Item_Trans` VALUES('9','zh', "薯條", "外表香脆，內在酥軟，搭配芥末醬真的是絕配", "點心", 'item009');
 INSERT INTO `Item_Trans` VALUES('10','zh', "珍珠鮮奶茶", "珍珠飽滿有彈性、嚼勁，唇齒留香，奶茶味道香甜，顏色好看", "飲料", 'item010');
+
+INSERT INTO `Item_Trans` VALUES('11','en', "Omurice", "The omelette rice made with soufflé. Very soft, and indescribably delicious", "Main", "item001");
+INSERT INTO `Item_Trans` VALUES('12','en', "Italian noodles", "Good taste with fresh grown tomatoes. Contains pork.", "Main", 'item002');
+INSERT INTO `Item_Trans` VALUES('13','en', "Chocolate cake", "Very soft and smooth ca", "Dessert", 'item003');
+INSERT INTO `Item_Trans` VALUES('14','en', "Fruit tea", "More ingridients than imagined, colorful multiple frut flavors", "Drink", 'item004');
+INSERT INTO `Item_Trans` VALUES('15','en', "Sesame noodles", "The dough is chewy, goes well with the sesame sauce at the bottom of the bowl. No sesame sauce heaviness", "Main", 'item005');
+INSERT INTO `Item_Trans` VALUES('16','en', "Pizza", "Freshly baked pizza with a thick layer of cheese", "Main", 'item006');
+INSERT INTO `Item_Trans` VALUES('17','en', "Puffs", "Looks like a large circle of smooth cream and jelly surrounding the puff. When you touch it with a spoon, you will find that it is actually crisp Marin sugar, and the mint jelly has a cool and sweet taste.", "Dessert", 'item007');
+INSERT INTO `Item_Trans` VALUES('18','en', "Onion rings", "Onion rings are not hot, but have a light fragrance. Every time I eat them, I feel a light salty and onion flavor. The two flavors are mixed together. With some seasonings and carbohydrates, the taste is indescribable.", "Dessert", 'item008');
+INSERT INTO `Item_Trans` VALUES('19','en', "Fries", "Crispy on the outside and soft on the inside, it''s a perfect match with mustard sauce", "Dessert", 'item009');
+INSERT INTO `Item_Trans` VALUES('20','en', "Pearl Fresh Milk Tea", "Very chewy pearls with sweet taste and beautiful color", "Beverage", 'item010');
+
 select * from `Item_Trans`;
 
